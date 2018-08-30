30d2be23f2f1245d26ebc88b428f9ae5f2388fc6
Omer <anjum.omer@gmail.com>
2018-08-30 14:48:20 -0500

    na

--- static/js/auth0-variables.js
+++ static/js/auth0-variables.js
@@ -1,4 +1,3 @@
-//var AUTH0_CLIENT_ID='anme9BlldFE2x8bfk8CDSU7z2mUhA9HR';
 var AUTH0_CLIENT_ID='this is single page client id I created. how it is connected to API is not clear. I believe these two are not connected as such';
 var AUTH0_DOMAIN='discvr.auth0.com'; 
 var AUTH0_CALLBACK_URL='http://localhost:3000/products'//location.href;
