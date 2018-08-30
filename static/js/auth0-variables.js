5cd1df2b434800cd1b68b07579497b9c59c86ff3
Omer <anjum.omer@gmail.com>
2018-08-30 14:43:53 -0500

    first commit

---
+++ static/js/auth0-variables.js
@@ -0,0 +1,5 @@
+//var AUTH0_CLIENT_ID='anme9BlldFE2x8bfk8CDSU7z2mUhA9HR';
+var AUTH0_CLIENT_ID='cqB7A_xFHd1FkaOOTcLo4yHF7_z1HLPU';
+var AUTH0_DOMAIN='discvr.auth0.com'; 
+var AUTH0_CALLBACK_URL='http://localhost:3000/products'//location.href;
+var AUTH0_API_AUDIENCE='discvr';
